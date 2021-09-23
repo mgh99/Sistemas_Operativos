01d90ee41cd06af0d9ca80e2d510c09fd9cc6fdf
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-09-23 20:41:08 +0200

    Create ejercicio_2.c

---
+++ Practica1/ejercicio_2.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h> //Librería para operaciones matemáticas
+
+int main(void) {
+	
+	pid_t childpid1, childpid2;
+	double array[20];
+	
+	childpid1 =fork(); //fork => crea un proceso
+	
+	if (childpid1 == -1) {
+		
+		perror("fork  no pudo crear el hijo1");
+		exit(1);		
+		
+	} else if (childpid1 == 0){
+			
+			printf("Soy el hijo1 con (PID %d) y voy a calcular el cuadrado de los pares\n", getpid());
+			for (int i = 0; i < 20; i++) {
+				if (i % 2 == 0) {
+					array[i] = pow(i, 2);
+					printf("%d: %.f, ", i, array[i]);
+					}
+			}		
+			
+	} else {
+		
+		childpid2 = fork();
+		
+		if (childpid2 == -1) {
+			
+			perror("fork no pudo crear el hijo2");
+			exit(1);
+			
+		} else if (childpid2 == 0) {
+			
+			printf("Soy el hijo2 con (PID %d) y voy a calcular el cuadrado de los impares\n", getpid());
+			for (int i = 0; i < 20; i++) {
+				if (i % 2!= 0) {
+					array[i] = pow(i, 2);
+					printf("%d %.f, ", i, array[i]);
+				}
+			}
+		} else {
+			
+			printf("Soy el padre con (PID %d) y voy a esperar a mis hijos\n", getpid());
+			childpid2 = wait(0);
+			childpid1 = wait(0);
+		}
+	}
+	
+	exit(0);
+}
